f6e546f2873d4cf3d5d54c463b4245d961dcdd5a
Luca Saccone <lu.saccone@gmail.com>
2021-09-20 14:37:56 +0200

    add a footer

--- src/app/components/component.module.ts
+++ src/app/components/component.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { FooterComponent } from './footer/footer.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { UnixTimestampPipe } from './pipe/unix-timestamp.pipe';
 import { WeatherForecastComponent } from './weather-forecast/weather-forecast.component';
@@ -12,7 +13,8 @@ import { WeatherComponent } from './weather/weather.component';
     WeatherForecastComponent,
     WeatherComponent,
     NavBarComponent,
-    UnixTimestampPipe
+    UnixTimestampPipe,
+    FooterComponent
   ],
   imports:[
     CommonModule,
@@ -20,7 +22,8 @@ import { WeatherComponent } from './weather/weather.component';
   ],
   exports: [
     WeatherComponent,
-    NavBarComponent
+    NavBarComponent,
+    FooterComponent
   ],
   providers: [],
   bootstrap: []
